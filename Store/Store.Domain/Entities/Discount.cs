a3bb084c78a59df5e15d3375e9548d000c0a20dc
Tiago Barbosa <tiago@farias.cc>
2022-03-27 14:40:45 -0300

    modeling domains and validations

---
+++ Store/Store.Domain/Entities/Discount.cs
@@ -0,0 +1,23 @@
+namespace Store.Domain.Entities;
+
+public class Discount : Entity
+{
+    public Discount(decimal amount, DateTime expireDate)
+    {
+        Amount = amount;
+        ExpireDate = expireDate;
+    }
+    
+    public decimal Amount { get; private set; }
+    public DateTime ExpireDate { get; private set; }
+
+    public bool IsValid()
+    {
+        return DateTime.Compare(DateTime.Now, ExpireDate) < 0;
+    }
+
+    public decimal Value()
+    {
+        return IsValid() ? Amount : 0;
+    }
+}
---
+++ Store/Store.Domain/Entities/OrderItem.cs
@@ -0,0 +1,27 @@
+using Flunt.Validations;
+
+namespace Store.Domain.Entities;
+
+public class OrderItem : Entity
+{
+    public OrderItem(Product product, int quantity)
+    {
+        AddNotifications(new Contract()
+            .Requires()
+            .IsNotNull(product, "Product", "Product invalid")
+            .IsGreaterThan(quantity,0,"Quantity", "Quantity is greater than zero."));
+        
+        Product = product;
+        Price = Product != null ? product.Price : 0;
+        Quantity = quantity;
+    }
+
+    public Product Product { get; private set; }
+    public decimal Price { get; private set; }
+    public int Quantity { get; private set; }
+
+    public decimal Total()
+    {
+        return Price * Quantity;
+    }
+}
