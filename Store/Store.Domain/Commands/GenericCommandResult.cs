8ab8d11e2cefa59aecb3e8a8f643e64a6f8e27ff
Tiago Barbosa <tiago@farias.cc>
2022-03-30 19:49:43 -0300

    created and testing handlers

--- Store/Store.Domain/Commands/GenericCommandResult.cs
+++ Store/Store.Domain/Commands/GenericCommandResult.cs
@@ -1,6 +1,8 @@
+using Store.Domain.Commands.Interfaces;
+
 namespace Store.Domain.Commands;
 
-public class GenericCommandResult
+public class GenericCommandResult : ICommandResult
 {
     public GenericCommandResult(bool success, string message, object data)
     {
